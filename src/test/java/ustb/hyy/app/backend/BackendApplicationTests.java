8934d6a345480b0f92f8614907b2fef2cbbcbf8c
hyy <645800585@qq.com>
2025-09-30 23:59:44 +0800

    first commit

---
+++ src/test/java/ustb/hyy/app/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ustb.hyy.app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
